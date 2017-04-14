8369085ff03f1b65c38afcc073f23ebd1b9660b9
Tony Nguyen <tntuyen@ucdavis.edu>
2017-04-13 23:01:50 -0700

    add mongod db, mongoose, and refactored routes to work with mongodb

--- app.js
+++ app.js
@@ -1,32 +1,60 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
+var mongoose = require('mongoose');
 
-app.use(bodyParser.urlencoded({extended: true}));
 
+
+mongoose.connect("mongodb://localhost:27017/yelp_camp")
 app.use(express.static('public'));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
-	var campgrounds = [
-		{ name: "Woodsfair", image: "http://www.photosforclass.com/download/7626464792" },
-		{ name: "Camp Palooza", image: "http://www.photosforclass.com/download/5641024448" },
-		{ name: "The Wilderness", image: "http://www.photosforclass.com/download/4369518024" },
-		{ name: "Woodsfair", image: "http://www.photosforclass.com/download/7626464792" },
-		{ name: "Camp Palooza", image: "http://www.photosforclass.com/download/5641024448" },
-		{ name: "The Wilderness", image: "http://www.photosforclass.com/download/4369518024" },
-		{ name: "Trees and Leaves n Stuff", image: "http://www.photosforclass.com/download/4368764673" },
-		{ name: "Trees and Leaves n Stuff", image: "http://www.photosforclass.com/download/4368764673" }
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+})
 
-	]
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+// 	{ 
+// 		name: "Camp Palooza", image: "http://www.photosforclass.com/download/5641024448" 
+// 	}, function(err, campground) {
+// 		if (err) {
+// 			console.log("Could not save campground");
+// 		} else {
+// 			console.log("Successfully created new campground:");
+// 			console.log(campground);
+// 		}
+// });
+
+	// var campgrounds = [
+	// 	{ name: "Camp Palooza", image: "http://www.photosforclass.com/download/5641024448" },
+	// 	{ name: "The Wilderness", image: "http://www.photosforclass.com/download/4369518024" },
+	// 	{ name: "Woodsfair", image: "http://www.photosforclass.com/download/7626464792" },
+	// 	{ name: "Camp Palooza", image: "http://www.photosforclass.com/download/5641024448" },
+	// 	{ name: "The Wilderness", image: "http://www.photosforclass.com/download/4369518024" },
+	// 	{ name: "Trees and Leaves n Stuff", image: "http://www.photosforclass.com/download/4368764673" },
+	// 	{ name: "Trees and Leaves n Stuff", image: "http://www.photosforclass.com/download/4368764673" }
+
+	// ]
 
 app.get('/', function(req, res) {
 	res.render('landing');
 })
 
 app.get('/campgrounds', function(req, res) {
-
-	res.render('campgrounds', {campgrounds: campgrounds} );
-})
+	// Get all campgrounds from db
+	Campground.find({}, function(err, allCampgrounds){
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('campgrounds', {campgrounds: allCampgrounds} );
+		}
+	});
+});
 
 app.get('/campgrounds/new', function(req, res) {
 	res.render('new');
@@ -37,10 +65,15 @@ app.post('/campgrounds', function(req, res) {
 	var name = req.body.name;
 	var image = req.body.image;
 	var newCampground = {name: name, image: image};
-	campgrounds.push(newCampground);
-	// create a new campground
+	// create a new campground and save to DB
+	Campground.create(newCampground, function(err, newlyCreated) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.redirect('campgrounds');
+		}
+	});
 	// redirect to campgrounds index
-	res.redirect('campgrounds');
 })
 
 app.listen(3000, function() {
