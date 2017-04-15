a6965d03955f42cfd156ef6bdced1bcfce7ea0a4
Tony Nguyen <tntuyen@ucdavis.edu>
2017-04-15 01:04:55 -0700

    commit before use auth

--- app.js
+++ app.js
@@ -7,12 +7,10 @@ var Comment = require('./models/comment');
 var seedDB = require('./seeds');
 
 // mongoose.connect("mongodb://localhost:27017/yelp_camp")
-// mongoose.connect("mongodb://tonyn4444:22444455a@ds043210.mlab.com:43210/yelpcampx")
 console.log(process.env.DATABASEURL);
 var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp";
 mongoose.connect(url);
 seedDB();
-// mongodb://tonyn4444:22444455a@ds043210.mlab.com:43210/yelpcampx
 app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
